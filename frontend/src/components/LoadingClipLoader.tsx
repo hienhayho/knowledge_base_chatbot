c34544cb89e82748b63b8ce3f4b6875d313029c1
Ho Trong Hien <115549171+hienhayho@users.noreply.github.com>
2024-12-19 12:03:59 +0700

    chore: improve UI/UX for chat page.

---
+++ frontend/src/components/LoadingClipLoader.tsx
@@ -0,0 +1,11 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const LoadingClipLoader = () => {
+    return (
+        <div className="flex justify-center items-center h-full m-auto">
+            <ClipLoader />
+        </div>
+    );
+};
+
+export default LoadingClipLoader;
